cd00a1d55ae1fe00c51e8d7fabb2b65e12dd7997
xuna <romainchampliau@gmail.com>
2021-10-20 21:42:51 +0200

    update

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] && s2[i] && i < n)
 		i++;
-	if (i < n)
-		return (s1[i] - s2[i]);
+	if (i <= n)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (0);
 }
--- libft.h
+++ libft.h
@@ -13,6 +13,8 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stddef.h>
+
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
